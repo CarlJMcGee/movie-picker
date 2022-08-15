c223e7a694a64568d72c1bbcd357b25c3c754a75
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-08-15 14:38:20 -0500

    css global color set

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-1": "#c5d5ea",
+        "blue-2": "#8f9eb2",
+        "blue-3": "#d7f3ff",
+        "brown-1": "#ffedcb",
+        "brown-2": "#6c3c00",
+      },
+    },
   },
   plugins: [],
 };
