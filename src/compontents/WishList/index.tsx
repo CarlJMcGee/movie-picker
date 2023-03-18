6de351e83c4c63e5f57231783eba7530ed7ee824
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2023-03-17 19:13:32 -0500

    wish list can now sort

--- src/compontents/WishList/index.tsx
+++ src/compontents/WishList/index.tsx
@@ -11,7 +11,7 @@ import Modal from "react-bootstrap/Modal";
 import Spinner from "react-bootstrap/Spinner";
 
 // mantine
-import { Autocomplete } from "@mantine/core";
+import { Autocomplete, NativeSelect, SegmentedControl } from "@mantine/core";
 
 // custom components
 import MovieInfoCard from "../MovieInfoCard";
@@ -28,7 +28,14 @@ export default function WishList({ unavailable, session }: IWishListProps) {
 
   // state
   const [showAddModal, setAddModal] = useState(false);
+  const [movies, setMovies] = useState(unavailable);
   const [movieTitle, setTitle] = useState("");
+  const [sortCat, setSortCat] = useState<
+    "Name" | "Score" | "Release Date" | "Length"
+  >("Name");
+  const [sortDir, setSortDir] = useState<"Ascending" | "Descending">(
+    "Ascending"
+  );
 
   // mutations
   const addMovie = trpc.useMutation(["movie.add"], {
@@ -50,6 +57,10 @@ export default function WishList({ unavailable, session }: IWishListProps) {
   };
 
   useEffect(() => {
+    setMovies(unavailable);
+  }, [unavailable]);
+
+  useEffect(() => {
     if (movieTitle === "") {
       return;
     }
@@ -61,6 +72,67 @@ export default function WishList({ unavailable, session }: IWishListProps) {
       clearTimeout(checkInput);
     };
   }, [movieTitle]);
+
+  useEffect(() => {
+    console.log(movies?.map((movie) => Number.parseInt(movie.Year)));
+
+    if (!Array.isArray(movies)) {
+      return;
+    }
+    switch (sortCat) {
+      case "Name":
+        setMovies(
+          [...movies].sort((movieA, movieB) => {
+            if (sortDir === "Ascending") {
+              return movieA.Title > movieB.Title ? 1 : -1;
+            }
+
+            return movieA.Title > movieB.Title ? -1 : 1;
+          })
+        );
+        break;
+      case "Score":
+        setMovies(
+          [...movies]?.sort((movieA, movieB) => {
+            if (movieA.Metascore === "N/A") {
+              return 1;
+            }
+
+            if (sortDir === "Ascending") {
+              return movieA.Metascore < movieB.Metascore ? -1 : 1;
+            }
+            return movieA.Metascore > movieB.Metascore ? -1 : 1;
+          })
+        );
+        break;
+      case "Release Date":
+        setMovies(
+          [...movies].sort((movieA, movieB) => {
+            const dateA = Number.parseInt(movieA.Year);
+            const dateB = Number.parseInt(movieB.Year);
+
+            if (sortDir === "Ascending") {
+              return dateA < dateB ? -1 : 1;
+            }
+            return dateA > dateB ? -1 : 1;
+          })
+        );
+        break;
+      case "Length":
+        setMovies(
+          [...movies].sort((movieA, movieB) => {
+            const lenA = Number.parseInt(movieA.Runtime.split(" ")[0]!);
+            const lenB = Number.parseInt(movieB.Runtime.split(" ")[0]!);
+
+            if (sortDir === "Ascending") {
+              return lenA > lenB ? 1 : -1;
+            }
+            return lenA < lenB ? 1 : -1;
+          })
+        );
+        break;
+    }
+  }, [sortCat, sortDir]);
 
   const searchTitles =
     movieTitle === ""
@@ -135,10 +207,30 @@ export default function WishList({ unavailable, session }: IWishListProps) {
           </Modal>
         </>
       )}
+      <div className="flex items-end">
+        <NativeSelect
+          label="Sort By"
+          value={sortCat}
+          data={["Name", "Score", "Release Date", "Length"]}
+          size={"xs"}
+          className="w-1/3 mb-2"
+          onChange={(e) =>
+            setSortCat(
+              e.target.value as "Name" | "Score" | "Release Date" | "Length"
+            )
+          }
+        />
+        <SegmentedControl
+          value={sortDir}
+          data={["Ascending", "Descending"]}
+          className="h-1/2 my-2 ml-2 bg-transparent"
+          onChange={(value) => setSortDir(value as "Ascending" | "Descending")}
+        />
+      </div>
       <Accordion>
         <AnimatePresence>
-          {Array.isArray(unavailable) &&
-            unavailable.map((movie) => (
+          {Array.isArray(movies) &&
+            movies.map((movie) => (
               <MovieInfoCard
                 movie={movie}
                 col="wish-list"
