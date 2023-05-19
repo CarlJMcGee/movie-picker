f4abf556983b59ecad7426163ec3229e1cca6657
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2023-05-19 10:38:08 -0500

    cool cat is responsive

--- src/compontents/CoolCatButton/index.tsx
+++ src/compontents/CoolCatButton/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import coolcatjpg from "./assets/cool cat head.png";
 import { trpc } from "../../utils/trpc";
@@ -18,17 +18,29 @@ export default function CoolCatButton() {
     setQuote(quotes[0]?.text);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setQuote(undefined);
+    }, 30 * 1000);
+  }, [quote]);
+
   return (
     <div className="flex justify-start mx-3 gap-4 items-start">
       <button
         onClick={() => fetchQuote()}
-        className={`hover:scale-90 ${coolCatThinking ? "animate-pulse" : ""}`}
+        className={`${coolCatThinking ? "animate-pulse" : "hover:scale-90"}`}
       >
         <Image src={coolcatjpg} alt="cool cat head" width={100} height={100} />
       </button>
-      {quote ? (
-        <p className="bg-white px-3 py-2 rounded-md rounded-bl-none">{quote}</p>
-      ) : null}
+      {/* {quote ? ( */}
+      <p
+        className={`bg-white px-3 py-2 rounded-md rounded-bl-none w-52 md:w-fit md:max-w-sm ${
+          quote ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        {quote}
+      </p>
+      {/* ) : null} */}
     </div>
   );
 }
